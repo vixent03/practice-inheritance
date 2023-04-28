990a56070e738a3cc7deb8924b2212ebb219ce9c
vi xent <alagovincent03@gmailcom>
2023-04-28 13:30:31 -0700

    first commit

--- classes/student.js
+++ classes/student.js
@@ -1,12 +1,31 @@
 const Person = require('./person');
 
+class Student extends Person {
+
+    constructor(major, GPA) {
+        this.major = 'major'
+        this.GPA = 'GPA'
+    }
+    super(firstName, lastName, age)
+
+    static compareGPA(student1, student2) {
+        if (student1.GPA > student2.GPA) {
+            return `${student1} has a higher GPA than ${student2}`;
+        } else if (student2.GPA > student1.GPA) {
+            return `${student2} has a higher GPA than ${student1}`;
+        } else if (student1.GPA === student2.GPA) {
+            return ` both has the same GPA`
+        }
+    }
+}
+
 // Your code here
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
 try {
-  module.exports = Student;
+    module.exports = Student;
 } catch {
-  module.exports = null;
+    module.exports = null;
 }
--- classes/teacher.js
+++ classes/teacher.js
@@ -2,11 +2,27 @@ const Person = require('./person');
 
 // Your code here
 
+class Teacher extends Person {
+    super(firstName, lastName, age)
+    constructor(subject, yearsOfExperience) {
+
+        this.subject = 'subject'
+        this.yearsOfExperience = 'yearsOfExperience'
+    }
+    static combinedYearsOfExperience(...Teacher) {
+        Teacher.forEach(Teacher => {
+            let sum = 0
+            let totalsum = sum += Teacher.YearsOfExperience
+            return totalsum
+        })
+    }
+}
+
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
 try {
-  module.exports = Teacher;
+    module.exports = Teacher;
 } catch {
-  module.exports = null;
+    module.exports = null;
 }
